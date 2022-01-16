4b3024c085688092467a6122f1c6f007a513aa73
Fagyi <fagyipda@gmail.com>
2022-01-16 09:35:05 +0100

    Revert "drm/msm/sde: Fix unchecked must_check DRM helper return values"
    This reverts commit 7b56f2a5881605541af9e41309fb0eb68778ff8d.

--- drivers/gpu/drm/msm/sde/sde_kms.c
+++ drivers/gpu/drm/msm/sde/sde_kms.c
@@ -2117,16 +2117,15 @@ static int sde_kms_set_crtc_for_conn(struct drm_device *dev,
 	crtc_state = drm_atomic_get_crtc_state(state, enc->crtc);
 	conn_state = drm_atomic_get_connector_state(state, conn);
 	if (IS_ERR(conn_state)) {
-		SDE_ERROR("error %ld getting crtc %d state\n",
-			  PTR_ERR(crtc_state), DRMID(conn));
+		SDE_ERROR("error %d getting connector %d state\n",
+				ret, DRMID(conn));
 		return -EINVAL;
 	}
 
 	crtc_state->active = true;
 	ret = drm_atomic_set_crtc_for_connector(conn_state, enc->crtc);
 	if (ret)
-		SDE_ERROR("error %d setting crtc for connector %d\n", ret,
-			  DRMID(conn));
+		SDE_ERROR("error %d setting the crtc\n", ret);
 
 	_sde_crtc_clear_dim_layers_v1(crtc_state);
 
@@ -2885,12 +2884,7 @@ retry:
 	if (ret)
 		goto end;
 
-	ret = drm_atomic_commit(state);
-	if (ret) {
-		SDE_ERROR("error %d committing state for connector %d\n", ret,
-			  DRMID(conn));
-		goto end;
-	}
+	drm_atomic_commit(state);
 end:
 	if (state)
 		drm_atomic_state_put(state);
