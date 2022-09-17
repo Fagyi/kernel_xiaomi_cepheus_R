3554ef1af9fb0a87224780280b57ef26e10a9d49
Fagyi <fagyipda@gmail.com>
2022-09-17 19:31:32 +0200

    schedutil: fix build

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -242,10 +242,6 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 		for_each_cpu(cpu, policy->cpus) {
 			trace_cpu_frequency(next_freq, cpu);
 		}
-	} else {
-		if (use_pelt())
-			sg_policy->work_in_progress = true;
-		sched_irq_work_queue(&sg_policy->irq_work);
 	}
 }
 
